e8557ac58dbabed4e2e98475bbddf7a026e712a0
czq58858800 <97653637@qq.com>
2015-12-28 06:54:25 +0800

    Initial Commit

---
+++ code/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  code
+//
+//  Created by zqchen on 15/12/28.
+//  Copyright © 2015年 zqchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ code/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  code
+//
+//  Created by zqchen on 15/12/28.
+//  Copyright © 2015年 zqchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
